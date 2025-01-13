891f1989757f3412e1093da543827cdde921e2a7
kushagraaery <66991958+kushagraaery@users.noreply.github.com>
2025-01-13 13:27:14 +0530

    Add files via upload

--- new.py
+++ new.py
@@ -601,7 +601,7 @@ def scheduled_job():
 def start_scheduler():
     # Create the scheduler and add the job
     scheduler = BackgroundScheduler()
-    scheduler.add_job(scheduled_job, 'cron', day_of_week='mon', hour=13, minute=7, timezone="Asia/Kolkata")
+    scheduler.add_job(scheduled_job, 'cron', day_of_week='mon', hour=13, minute=30, timezone="Asia/Kolkata")
     # Start the scheduler
     scheduler.start()
 
@@ -609,4 +609,4 @@ def start_scheduler():
 if __name__ == "__main__":
     threading.Thread(target=start_scheduler, daemon=True).start()
 
-st.write("updated 3")
+st.write("updated 4")
