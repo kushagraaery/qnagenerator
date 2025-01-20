e3e02c117612f4dfcfc2e7e3ad5f3ae01abcbebd
kushagraaery <66991958+kushagraaery@users.noreply.github.com>
2025-01-20 11:54:29 +0530

    Update new.py

--- new.py
+++ new.py
@@ -721,12 +721,10 @@ def scheduled_job():
 def start_scheduler():
     # Create the scheduler and add the job
     scheduler = BackgroundScheduler()
-    scheduler.add_job(scheduled_job, 'cron', day_of_week='mon', hour=11, minute=50, timezone="Asia/Kolkata")
+    scheduler.add_job(scheduled_job, 'cron', day_of_week='mon', hour=10, minute=00, timezone="Asia/Kolkata")
     # Start the scheduler
     scheduler.start()
 
 # Start the scheduler in a separate thread
 if __name__ == "__main__":
     threading.Thread(target=start_scheduler, daemon=True).start()
-
-st.write("updated")
